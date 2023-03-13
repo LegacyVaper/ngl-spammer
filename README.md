5af0495628a4eeca58de72b5bbc9b8f1a6120f75
LegacyVaper <127703050+LegacyVaper@users.noreply.github.com>
2023-03-13 16:38:13 +0100

    Main

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# ngl-spammer
-ngl.link message spammer
+# 📧 NGL.link Message Bot
+
+This is a simple JavaScript code that allows users to send messages to NGL.link users using their API. The bot works by sending a POST request to the NGL.link API with the specified message and the user's username.
+
+
+## 🚀 Installation
+
+To use this message bot, simply download the ngl-message-bot.js file and include it in your project. You will also need to have the crypto module installed. You can install it using the following command:
+
+```
+npm install crypto
+```
+
+
+## 🎉 Usage
+
+To use this message bot, simply call the sendMessage function with the desired username and message as arguments. For example:
+
+```
+const { sendMessage } = require("./ngl-message-bot.js");
+
+sendMessage("example_user", "Example Message");
+```
+
+
+
+⚠️ Please note that sending messages to NGL.link users without their consent is not recommended and may be illegal in some jurisdictions. Use this code at your own risk.
---
+++ ngl-message-bot.js.js
@@ -0,0 +1,49 @@
+const crypto = require("crypto");
+
+let counter = 0;
+
+const sendMessage = async (username, message) => {
+    while (true) {
+        try {
+            const date = new Date();
+            const minutes = date.getMinutes();
+            const hours = date.getHours();
+            const formattedDate = `${hours}:${minutes}`;
+
+            const deviceId = crypto.randomBytes(21).toString("hex");
+            const url = "https://ngl.link/api/submit";
+            const headers = {
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/109.0",
+                "Accept": "*/*",
+                "Accept-Language": "en-US,en;q=0.5",
+                "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+                "X-Requested-With": "XMLHttpRequest",
+                "Sec-Fetch-Dest": "empty",
+                "Sec-Fetch-Mode": "cors",
+                "Sec-Fetch-Site": "same-origin",
+                "Referer": `https://ngl.link/${username}`,
+                "Origin": "https://ngl.link"
+            };
+            const body = `username=${username}&question=${message}&deviceId=${deviceId}&gameSlug=&referrer=`;
+
+            const response = await fetch(url, {
+                method: "POST",
+                headers,
+                body,
+                mode: "cors",
+                credentials: "include"
+            });
+
+            if (response.status !== 200) {
+                console.log(`[${formattedDate}] [Err] Ratelimited`);
+                await new Promise(resolve => setTimeout(resolve, 25000));
+            } else {
+                counter++;
+                console.log(`[${formattedDate}] [Msg] Sent: ${counter}`);
+            }
+        } catch (error) {
+            console.error(`[${formattedDate}] [Err] ${error}`);
+            await new Promise(resolve => setTimeout(resolve, 5000));
+        }
+    }
+};
